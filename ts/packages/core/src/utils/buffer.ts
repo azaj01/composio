233d99d65f01c2e51e029f88520fe2771100d4bc
jkomyno <12381818+jkomyno@users.noreply.github.com>
2026-01-22 18:47:20 +0400

    feat(ts): [breaking] remove node:crypto usage from triggers, composio.triggers.verifyWebhook is now async

---
+++ ts/packages/core/src/utils/buffer.ts
@@ -0,0 +1,11 @@
+/**
+ * Convert ArrayBuffer to base64.
+ */
+export function arrayBufferToBase64(buffer: ArrayBuffer) {
+  const bytes = new Uint8Array(buffer);
+  let binary = '';
+  for (let i = 0; i < bytes.length; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
