c0ae64b1f032f271240a9108f101487fc6a5fbd0
Sushmithamallesh <sushdec6@gmail.com>
2026-01-22 12:46:26 -0800

    Add API proxy to fix CORS issues in API reference playground

--- docs/components/api-page.client.tsx
+++ docs/components/api-page.client.tsx
@@ -1,4 +1,6 @@
 'use client';
 import { defineClientConfig } from 'fumadocs-openapi/ui/client';
 
-export default defineClientConfig({});
+export default defineClientConfig({
+  proxyUrl: '/api/proxy',
+});
